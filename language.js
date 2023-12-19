73a8508dec4b269b6c285214ccce83229fce259d
steven <stevenhwt@163.com>
2023-12-19 21:36:42 +0800

    update language.js

--- language.js
+++ language.js
@@ -1,27 +1,18 @@
 function switchLanguage() {
+    // 检查当前存储的语言，如果没有设置，默认为英文
     var currentLanguage = localStorage.getItem('language') || 'english';
+
+    // 切换语言设置
     var newLanguage = currentLanguage === 'english' ? 'chinese' : 'english';
+
+    // 更新存储的语言
     localStorage.setItem('language', newLanguage);
 
-    var elementsEn = document.querySelectorAll('[data-lang="en"]');
-    var elementsZh = document.querySelectorAll('[data-lang="zh"]');
-
-    elementsEn.forEach(function(el) {
-        el.style.display = (newLanguage === 'chinese') ? 'none' : '';
-    });
-
-    elementsZh.forEach(function(el) {
-        el.style.display = (newLanguage === 'chinese') ? '' : 'none';
-    });
+    // 应用新的语言设置到页面元素
+    updateLanguageOnPage(newLanguage);
 }
 
-// 当页面加载时，应用存储的语言偏好
-document.addEventListener('DOMContentLoaded', function() {
-    var storedLanguage = localStorage.getItem('language') || 'english';
-    switchLanguageTo(storedLanguage);
-});
-
-function switchLanguageTo(language) {
+function updateLanguageOnPage(language) {
     var elementsEn = document.querySelectorAll('[data-lang="en"]');
     var elementsZh = document.querySelectorAll('[data-lang="zh"]');
 
@@ -33,3 +24,10 @@ function switchLanguageTo(language) {
         el.style.display = (language === 'chinese') ? '' : 'none';
     });
 }
+
+// 当页面加载时，应用存储的语言偏好
+document.addEventListener('DOMContentLoaded', function() {
+    // 获取存储的语言偏好或默认为英文
+    var storedLanguage = localStorage.getItem('language') || 'english';
+    updateLanguageOnPage(storedLanguage);
+});
